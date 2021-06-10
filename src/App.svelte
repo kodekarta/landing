0e55bdccf4a1b04ff76a019c34f345c1b9e1b451
Fajar Ru <kzofajar@gmail.com>
2021-06-10 11:46:05 +0700

    Update main page

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,34 @@
   import "tailwindcss/tailwind.css"
 </script>
 
-<main>
-    <div class="flex h-screen">
-        <h1 class="text-2xl m-auto">It Works!</h1>
+<main style="background-color: #f7f7f7;">
+    <div class="bg-white">
+        <section id="hero" class="max-h-screen">
+            <div class="max-w-7xl mx-auto py-4 pt-24 px-4 md:px-8 text-center text-gray-700">
+                <h1 class="text-center text-5xl font-semibold">
+                    Saatnya skill kodingmu <span class="text-blue-500">naik level</span>
+                </h1>
+                <p class="my-4 text-xl">Selesaikan course programmingmu, mari bikin project bersamaku</p>
+                <div class="my-8">
+                    <a href="#" class="inline-block py-3 px-6 bg-blue-500 transition hover:bg-blue-600 border border-blue-600 text-white rounded-lg shadow">
+                        Segera Datang!
+                    </a>
+                </div>
+                <div class="flex justify-center">
+                    <img src="img/illustration/coding.svg" alt="Kodekarta" class="min-w-full md:w-2/3 md:h-2/3">
+                </div>
+            </div>
+        </section>
     </div>
+
+    <section id="partners" class="py-4 text-center">
+        <div class="flex flex-row justify-center space-x-4 h-8 md:h-12 mt-4">
+            <img class="object-scale-down" src="img/partners/abp.png" alt="Amikom Business Park">
+            <img class="object-scale-down" src="img/partners/howbizz.png" alt="Howbizz">
+        </div>
+    </section>
+
+    <footer id="footer" class="text-center p-6">
+        &copy; 2021 Kodekarta from Howbizz. All rights reserved.<br>
+    </footer>
 </main>
